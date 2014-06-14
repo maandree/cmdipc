ad3c8aa98b9612b8358d9751fb73d99e33b0c93c
Mattias Andrée <maandree@operamail.com>
2014-06-14 19:06:18 +0200

    common api for posix and sysv ipc
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/unified_posix_ipc.py
@@ -0,0 +1,76 @@
+# -*- python -*-
+'''
+cmdipc — System V and POSIX IPC from the command line
+Copyright © 2014  Mattias Andrée (maandree@member.fsf.org)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+import posix_ipc # http://semanchuk.com/philip/posix_ipc
+
+
+SignalError = posix_ipc.SignalError
+PermissionsError = posix_ipc.PermissionsError
+ExistentialError = posix_ipc.ExistentialError
+BusyError = posix_ipc.BusyError
+
+CREAT = posix_ipc.O_CREAT
+EXCL = posix_ipc.O_EXCL
+
+def keycat(*keys):
+    return ''.join(keys)
+
+def keysep(keys):
+    return ['/' + key for key in keys[1:].split('/')]
+
+class Semaphore(posix_ipc.Semaphore):
+    def __init__(self, *args, **kwargs):
+        posix_ipc.Semaphore.__init__(self, *args, **kwargs)
+    def P(timeout = None):
+        self.acquire(timeout)
+    def V():
+        self.release()
+    def set_value(self, value):
+        for _ in range(value):
+            self.V()
+    def remove(self):
+        self.unlink()
+
+class SharedMemory(posix_ipc.SharedMemory):
+    def __init__(self, *args, **kwargs):
+        posix_ipc.SharedMemory.__init__(self, *args, **kwargs)
+    def read(self, byte_count = 0, offset = 0):
+        rc = []
+        byte_count = min(byte_count, self.size - offset)
+        os.lseek(self.fd, offset, os.SEEK_SET)
+        while len(rc) < byte_count:
+            rc += list(os.read(self.fd), byte_count - len(rc))
+        return bytes(rc)
+    def write(self, s, offset = 0):
+        s = s[:min(len(s), self.size - offset)]
+        os.lseek(self.fd, offset, os.SEEK_SET)
+        while len(s) > 0:
+            s = s[os.write(self.fd, s):]
+    def close(self):
+        self.close_fd()
+    def remove(self):
+        self.unlink()
+
+class MessageQueue(posix_ipc.MessageQueue):
+    def __init__(self, *args, **kwargs):
+        posix_ipc.MessageQueue.__init__(self, *args, **kwargs)
+    def remove(self):
+        self.unlink()
+
---
+++ src/unified_sysv_ipc.py
@@ -0,0 +1,65 @@
+# -*- python -*-
+'''
+cmdipc — System V and POSIX IPC from the command line
+Copyright © 2014  Mattias Andrée (maandree@member.fsf.org)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import sysv_ipc # http://semanchuk.com/philip/sysv_ipc
+
+
+ftok = sysv_ipc.ftok
+
+PermissionsError = sysv_ipc.PermissionsError
+ExistentialError = sysv_ipc.ExistentialError
+BusyError = sysv_ipc.BusyError
+class SignalError(Exception):
+    pass
+
+CREAT = sysv_ipc.IPC_CREAT
+EXCL = sysv_ipc.IPC_EXCL
+
+def keycat(*keys):
+    return '.'.join([str(key) for key in keys])
+
+def keysep(keys):
+    return [int(key) for key in keys.split('.')]
+
+class Semaphore(sysv_ipc.Semaphore):
+    def __init__(self, *args, **kwargs):
+        sysv_ipc.Semaphore.__init__(self, *args, **kwargs)
+    def set_value(self, value):
+        self.value = value
+    def close(self):
+        pass
+
+class SharedMemory(sysv_ipc.SharedMemory):
+    def __init__(self, *args, **kwargs):
+        sysv_ipc.SharedMemory.__init__(self, *args, **kwargs)
+    def close(self):
+        pass
+
+class MessageQueue(sysv_ipc.MessageQueue):
+    def __init__(self, *args, **kwargs):
+        sysv_ipc.MessageQueue.__init__(self, *args, **kwargs)
+        self.send_ = sysv_ipc.MessageQueue.send
+        self.recv_ = sysv_ipc.MessageQueue.receive
+    def send(self, message, timeout = None, type = 1):
+        self.send_(self, message, (timeout is None) or (timeout != 0), type)
+    def receive(self, timeout = None, type = 1):
+        self.recv_(self, (timeout is None) or (timeout != 0), type)
+    def close(self):
+        pass
+
